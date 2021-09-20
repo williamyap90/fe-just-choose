7e8bd09f0e8fc073982b74b548aaff711e5af6e2
Sgavinmills <sgavinmills@gmail.com>
2021-09-20 17:03:48 +0100

    api requests updated

--- src/Components/EventConfirmation.tsx
+++ src/Components/EventConfirmation.tsx
@@ -1,7 +1,34 @@
-export default function EventConfirmation() {
+import { useEffect, useState } from "react"
+import { fetchEvent } from "../API-Funcs/API";
+import Loading from "./Loading";
+
+interface Props {
+    eventName: string;
+    setEventName: (val: string) => void;
+}
+export default function EventConfirmation(props : Props) {
+    const [event, setEvent] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        //api call
+        setIsLoading(true);
+        fetchEvent(props.eventName)
+        .then((response: any) => {
+            setEvent(response)
+            setIsLoading(false);
+        })
+        .catch((e) => {
+            console.log(e);
+        })
+    }, [])
+
+    if(isLoading) 
+    return <Loading />
     return (
         <div>
-            CONFIRMATION PAGE
+            {/* <p>THanks {event.organiser} for creating your event.</p> */}
+            {/* <p>VOting will end at: {event.endDate}</p> */}
+            <p>Here is your URL. PLease share with your friends</p>
         </div>
     )
 }
---
+++ src/Utils/Utils.ts
@@ -0,0 +1,54 @@
+export const extractRestaurantInfo = (shortlist: any) => {
+
+    const newShortList = shortlist.map((restObj: any) => {
+        const newObj: any = {};
+        for(const property in restObj) {
+            if(property === 'categories') {
+                const categoryArray = restObj.categories.map((catObj: any) => {
+                    return catObj.title;
+                })
+                newObj['categories'] = categoryArray;
+            }
+
+            if(property === 'coordinates') {
+                newObj['coordinates'] = {...restObj.coordinates}
+            }
+
+            if(property === 'display_phone') {
+                newObj['display_phone'] = restObj.display_phone;
+            }
+
+            if(property === 'distance') {
+                newObj['distance'] = restObj.distance;
+            }
+
+            if(property === 'image_url') {
+                newObj['image_url'] = restObj.image_url;
+            }
+            if(property === 'name') {
+                newObj['name'] = restObj.name;
+            }
+            if(property === 'price') {
+                newObj['price'] = restObj.price;
+            }
+            if(property === 'rating') {
+                newObj['rating'] = restObj.rating;
+            }
+            if(property === 'review_count') {
+                newObj['review_count'] = restObj.review_count;
+            }
+            if(property === 'url') {
+                newObj['url'] = restObj.url;
+            }
+            if(property === 'location') {
+                if(restObj.location) {
+                    newObj['display_address'] = [...restObj.location.display_address]
+                }
+            }
+
+        }
+
+        return newObj;
+    })
+    return newShortList
+}
