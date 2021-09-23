c8153795126ba9918cfc23e8a7aa7a1b8e76701e
williamyap90 <william.yap90@gmail.com>
2021-09-23 06:52:39 +0100

    fix styling for login/signup pages

--- src/Components/SignUpPage.tsx
+++ src/Components/SignUpPage.tsx
@@ -3,23 +3,48 @@ import { Link } from 'react-router-dom';
 const SignUpPage = () => {
     return (
         <section className="UserForm">
-            <h1>Sign Up</h1>
-            <form>
-                <label htmlFor="fullName">Full Name</label>
-                <input type="text" name="fullName" id="fullName" />
-                <label htmlFor="email"> Email </label>
-                <input type="email" name="email" id="email" />
-                <label htmlFor="passwordField"> Password </label>
+            <h1 className="page-header page-header-signup">Sign Up</h1>
+            <form className="signup-form">
+                <label className="input-label" htmlFor="fullName">
+                    Full Name
+                </label>
                 <input
+                    className="input-textbox"
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                />
+                <label className="input-label" htmlFor="email">
+                    {' '}
+                    Email{' '}
+                </label>
+                <input
+                    className="input-textbox"
+                    type="email"
+                    name="email"
+                    id="email"
+                />
+                <label className="input-label" htmlFor="passwordField">
+                    {' '}
+                    Password{' '}
+                </label>
+                <input
+                    className="input-textbox"
                     type="password"
                     name="passwordField"
                     id="passwordField"
                 />
-                <input type="button" value="Sign Up" />
+                <input
+                    className="secondary-button secondary-button-login"
+                    type="button"
+                    value="Sign Up"
+                />
             </form>
             {/* Below Paragraph needs to be linked in Sign Up section */}
-            <Link to="/login-page">
-                <p>Already have an account? Login</p>
+            <Link to="/login-page" style={{ textDecoration: 'none' }}>
+                <p className="footer-signup">
+                    Already have an account? <span>Login</span>
+                </p>
             </Link>
         </section>
     );
