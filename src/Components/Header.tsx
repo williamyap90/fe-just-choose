60a518dd068c23b0bf72e04bfbc263ba092e4596
Ammar Hassan <ammar@ammarhassan.co.uk>
2021-09-23 01:45:31 +0100

    updated class to match

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,10 +3,10 @@
 const Header = () => {
     return (
     
-        <div className="Header title">
-            <h1 className="has-text-warning">Welcome to
+        <div className="Header title is-centered">
+            <h1 className="has-text-black-bis is-size-5">Welcome to
                 <br />
-            <span className="has-text-weight-bold has-text-link-dark">Just Choose</span>
+            <span className="has-text-weight-bold has-text-link-dark is-size-3">Just Choose</span>
             </h1>
         </div>
 
