f127a60ad650bfce43219bc643533e6d95010182
Sgavinmills <sgavinmills@gmail.com>
2021-09-23 11:00:48 +0100

    create user func

--- src/Components/SignUpPage.tsx
+++ src/Components/SignUpPage.tsx
@@ -1,18 +1,70 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { isGetAccessorDeclaration } from 'typescript';
+import { postUser } from '../API-Funcs/API';
+import { useHistory } from 'react-router-dom';
+
 
 const SignUpPage = () => {
+
+    const [formInput, setFormInput] = useState<any>({
+        firstName : '',
+        lastName : '',
+        email : '',
+        password : '',
+        confirmPassword : '',
+    });
+
+    const handleChange = (e: any) => {
+        setFormInput((currFormInput: any) => {
+            const newFormInput = {...currFormInput};
+            newFormInput[e.target.id] = e.target.value;
+            return newFormInput;
+        })
+    }
+    const history = useHistory();
+    const goLogin = () => {
+        history.push('/login-page');
+    };
+
+    const handleSubmit = (e: any) => {
+        console.log('submitting');
+        e.preventDefault();
+        if(formInput.firstName.length >= 2 && formInput.lastName.length >= 2 && formInput.email.length >= 4 && formInput.password.length >= 5 && formInput.password === formInput.confirmPassword ) {
+           console.log('Creating user....')
+            postUser(formInput).then((response) => {
+                goLogin();
+            });
+        } else {
+            console.log('Form not filled in properly')
+        }
+    }
+    console.log(formInput);
     return (
         <section className="UserForm">
             <h1 className="page-header page-header-signup">Sign Up</h1>
-            <form className="signup-form">
-                <label className="input-label" htmlFor="fullName">
-                    Full Name
+            <form onSubmit={handleSubmit} className="signup-form">
+                <label className="input-label" htmlFor="firstName">
+                    First Name
                 </label>
                 <input
                     className="input-textbox"
                     type="text"
-                    name="fullName"
-                    id="fullName"
+                    name="firstName"
+                    id="firstName"
+                    value={formInput.firstName}
+                    onChange={handleChange}
+                />
+                <label className="input-label" htmlFor="lastName">
+                Last Name
+                </label>
+                <input
+                    className="input-textbox"
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                    value={formInput.lastName}
+                    onChange={handleChange}
                 />
                 <label className="input-label" htmlFor="email">
                     {' '}
@@ -23,20 +75,36 @@ const SignUpPage = () => {
                     type="email"
                     name="email"
                     id="email"
+                    value={formInput.email}
+                    onChange={handleChange}
                 />
-                <label className="input-label" htmlFor="passwordField">
+                <label className="input-label" htmlFor="password">
                     {' '}
                     Password{' '}
                 </label>
                 <input
                     className="input-textbox"
                     type="password"
-                    name="passwordField"
-                    id="passwordField"
+                    name="password"
+                    id="password"
+                    value={formInput.password}
+                    onChange={handleChange}
+                />
+                   <label className="input-label" htmlFor="confirmPassword">
+                    {' '}
+                    Confirm Password{' '}
+                </label>
+                <input
+                    className="input-textbox"
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    value={formInput.confirmPassword}
+                    onChange={handleChange}
                 />
                 <input
                     className="secondary-button secondary-button-login"
-                    type="button"
+                    type="submit"
                     value="Sign Up"
                 />
             </form>
